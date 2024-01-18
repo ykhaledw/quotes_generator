35d12ea71054dd2d97f9664388ee1662064cd9fe
ykhaledw <youssefwahdan12@gmail.com>
2024-01-18 15:54:26 +0200

    finish quote cubit

--- lib/presentation/views/manager/cubits/generate_quote_cubit/quote_cubit.dart
+++ lib/presentation/views/manager/cubits/generate_quote_cubit/quote_cubit.dart
@@ -1,8 +1,21 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:quote_generator_app/core/utils/quote_service.dart';
 
 part 'quote_state.dart';
 
 class QuoteCubit extends Cubit<GenerateQuoteState> {
-  QuoteCubit() : super(GenerateQuoteInitial());
+  QuoteCubit(this.quoteService) : super(GenerateQuoteInitial());
+  final QuoteService quoteService;
+
+  Future generateQuote() async {
+    emit(GenerateQuoteLoading());
+    try {
+      var data = await quoteService.getQuote();
+      emit(GenerateQuoteSuccess());
+      return data;
+    } catch (e) {
+      emit(GenerateQuoteFailure(errorMessage: e.toString()));
+    }
+  }
 }
